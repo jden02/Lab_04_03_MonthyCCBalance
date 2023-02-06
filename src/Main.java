052206bc212afc820b435f66cafa6047528cb947
jden02 <jden71002@gmail.com>
2023-02-05 19:57:51 -0500

    Task 3 Complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double balance = 5000;
+        double intRate = 0.17;
+        double interestDue;
+
+        //one month
+        interestDue = balance*intRate;
+        System.out.println("Total due after one month: " + interestDue);
+
+        //two months
+        interestDue = (interestDue+balance) * intRate;
+        System.out.println("Total due after two months: " + interestDue);
+
     }
 }
